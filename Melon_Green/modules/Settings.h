05d1ea48ee355ca7ab1622785f85d983cf5c477c
M. H. Golkar <1233337+mhgolkar@users.noreply.github.com>
2018-07-02 16:49:25 +0430

    v1.2.0

--- Melon_Green/modules/Settings.h
+++ Melon_Green/modules/Settings.h
@@ -1,7 +1,7 @@
 /*
- * Melon Green v1.0.0
+ * Melon Green v1.2.0
  * Morteza H. Golkar
- * 2017
+ * 2018
  * -----------------------------
  * Settings
  * -----------------------------
@@ -9,11 +9,11 @@
 
 /* Note:
  * All Settings Are Accessible from Serial CLI
- * ( Except: <Sensors>, DiagMaxMilliSecond & century )
+ * ( Except: "Sensors" group, DiagMaxMilliSecond & century )
 */
- 
+
 // Sensors
-	#define RTCMODULE RTC_DS3231 // Real Time Clock Module Type: RTC_DS3231, RTC_DS1307, RTC_PCF8523, RTC_PCF8563
+	#define RTCMODULE RTC_DS1307 // Real Time Clock Module Type: RTC_DS3231, RTC_DS1307, RTC_PCF8523, RTC_PCF8563
 	#define DHTTYPE DHT11 // Humidity & Temperature Module: DHT22 (AM2302), DHT21, DHT11 (AM2301)
 // Duty Cycle
 	#define DutyCycle 0 // x Cycles... [0 = Sleepless]
@@ -38,10 +38,11 @@
 	#define IrrigateIntervalUnit 1 // [0 = Minutes, 1= Hours, 2 = Days, 3 = Week, 4 = Month]
 	#define IrrigationOnTemperatureHigherThan 0 //'C [0->OFF]
 	#define IrrigationOnHumidityBelow 100 //% Relative [100->OFF]
-	#define IrrigateAtDaytimeOnly  1 // [0 = OFF]
+	#define IrrigateAtDaytimeOnly  0 // [0 = OFF]
 	#define DontIrrigateOnRainyDays 1 // [0 = OFF]
 	#define IrrigateToMaximumSoilMoisture 40 // % Maximum Allowable Soil Moisture
 	#define IrrigationDurationEachTime  2 // minutes
+	#define MaximumIrrigationPerDay  60 // minutes
 // Grow Light Supplier
 	#define GrowLightIntervalsEvery 0 //[0 = OFF]
 	#define GrowLightIntervalUnit 1 // [0 = Minutes, 1= Hours, 2 = Days, 3 = Week, 4 = Month]
@@ -51,8 +52,10 @@
 	#define DontSupplyGrowLightAfter 24 // 0-23 O'Clock [> 23 -> OFF]
 // Temperature & Humidity
 	#define KeepTemperatureAbout 0 //[0 = OFF]
+	#define KeepTemperatureNight 0 //[0 = OFF]
 	#define TemperatureTolerance 5 //'C
 	#define keepHumidityAbout 0 //[0 = OFF]
+	#define keepHumidityNight 0 //[0 = OFF]
 	#define HumidityTolerance 10 //%
 	#define UseFanCoolerToReduceHumidity 0 // [0] NO [1] YES
 // Feeding Solution Mixer
